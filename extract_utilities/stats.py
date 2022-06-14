55626cc43330d54f388e156c3e4cbe811a61b28e
Mike Nestel <nestelm@gmail.com>
2022-06-14 12:23:14 -0500

    add more stats

--- extract_utilities/stats.py
+++ extract_utilities/stats.py
@@ -1,5 +1,6 @@
 from extract_utilities import nhl_data_requests as ndr
 import pandas as pd
+import numpy as np
 
 import settings
 import progress_bar as pb
@@ -8,66 +9,75 @@ import uuid
 
 
 class Stats:
-    def __init__(self, data_since, situational=False, collect=False, expand=None) -> None:
+    def __init__(self, stats_types=None, data_since=None, collect=None, expand=None, season_level_request=None) -> None:
         self.data_since = data_since
-        self.situational = situational
+        self.stats_types = stats_types
         self.collect = collect
         self.expand = expand
+        self.season_level_request = season_level_request
     files = settings.get_data_file_paths()
     if "metadata.csv" not in files:
         raise Exception("Please run roster metadata")
+    if "rosters.csv" not in files:
+        raise Exception("Please run rosters")
     player_metadata = pd.read_csv(files["metadata.csv"], usecols=["id", "primaryPosition_code"])
+    player_seasons = pd.read_csv("data/rosters/rosters.csv", usecols=["person_id", "season"]) \
+        .groupby("person_id").agg({"season": [np.min, np.max]}).reset_index()
  
-    def all_player_stats(self, player_type):
+    def all_player_stats(self, player_type, level="season_by_season"):
         if player_type not in ["skater", "goalie"]:
             raise Exception("Please specifiy skater or goalie stat type")
         if player_type == "skater":
             mask = ['C', 'L', 'D', 'R']
         if player_type == "goalie":
             mask = ['G']
-        
-        directory = "data/stats"
-        temp_directory = f"data/stats/{player_type}"
 
         players = list(self.player_metadata.loc[self.player_metadata["primaryPosition_code"].isin(mask)]["id"].unique())
         n = len(players)
 
         for i, player in enumerate(players):
-            player_stats = (pb.report(i, n), self.player_stats(player))[1]
-            settings.write_df_to_csv(player_stats, temp_directory, f"{player}.csv")
-        player_df = pd.concat([pd.read_csv(file)
-                               for file in settings.wildcard_files(f"{temp_directory}/*.csv")])
-        settings.write_df_to_csv(player_df, directory, f"{player_type}s.csv")
-        settings.remove_folder(temp_directory)
+            pb.report(i, n)
+            if self.season_level_request:
+                self.player_stats_all_seasons(player)
+            else:
+                self.player_stats(player)
 
+        if self.collect:
+            for stat_type in self.stats_types:
+                directory = f"data/stats/{stat_type}"
+                temp_directory = f"data/stats/{stat_type}_temp"
+                settings.temp_files_to_df(wildcard="*.csv", fp_temp=temp_directory, fp_final=directory, fn=player_type + "s")
         return True
-        
-    def player_stats(self, player_id, stats_type="yearByYear"):
-        url = f"https://statsapi.web.nhl.com/api/v1/people/{player_id}/stats?stats={stats_type}"
-        body = ndr.get_request(url)
 
-        stats = pd.concat([pd.json_normalize(b, sep="_") for b in body["stats"][0]["splits"]])
-        stats.columns = stats.columns.str.replace("stat_", "")
-        stats["player_id"] = player_id
-
-        if self.situational:
-            seasons = stats["season"].unique()
-            situational_stats = self.player_situational_goals(player_id, seasons)
-            if isinstance(situational_stats, pd.DataFrame):
-                return stats.join(situational_stats, on="season", how="left", rsuffix="situational")
-        return stats
-
-    def player_situational_goals(self, player_id, seasons):
-        situational_list = []
+    def player_stats_all_seasons(self, player_id):
+        player = self.player_seasons[self.player_seasons["person_id"].isin([player_id])]
+        seasons = ndr.generate_season_array(str(player.season.amin.values[0]), str(player.season.amax.values[0]))
         for season in seasons:
-            url = f"https://statsapi.web.nhl.com/api/v1/people/{player_id}/stats?stats=goalsByGameSituation&season={season}"
-            body = ndr.get_request(url)
-            if body["stats"][0]["splits"]:
-                situational_df = pd.DataFrame(body["stats"][0]["splits"][0]["stat"], index=["season"])
-                situational_df["season"] = season
-                situational_list.append(situational_df)
-        if situational_list:
-            return pd.concat(situational_list).set_index("season")
+            self._player_stats_by_season(player_id=player_id, season=season)
+                       
+    def _player_stats_by_season(self, player_id, season):
+        stats_types = ",".join(self.stats_types) if isinstance(self.stats_types, list) else self.stats_types
+        url = f"https://statsapi.web.nhl.com/api/v1/people/{player_id}/stats/?stats={stats_types}&season={season}"
+        body = ndr.get_request(url)
+        if not body:
+            return None
+        stats = {b["type"]["displayName"]: self._sub_df_transform(player_id=player_id, splits=b["splits"])
+                 for b in body["stats"] if b and b.get("type", False)}
+        
+        for k, v in stats.items():
+            settings.write_df_to_storage(df=v, fp=f"data/stats/{k}_temp", fn=f"{player_id}_{season}", file_type="csv")
+        return True
+
+    def player_stats(self, player_id):
+        stats_types = ",".join(self.stats_types) if isinstance(self.stats_types, list) else self.stats_types
+        url = f"https://statsapi.web.nhl.com/api/v1/people/{player_id}/stats?stats={stats_types}"
+        body = ndr.get_request(url)
+        stats = {b["type"]["displayName"]: self._sub_df_transform(player_id=player_id, splits=b["splits"])
+                 for b in body["stats"] if b and b.get("type", False)}
+
+        for k, v in stats.items():
+            settings.write_df_to_storage(df=v, fp=f"data/stats/{k}_temp", fn=f"{player_id}", file_type="csv")
+        return True
 
     def _gamepks(self):
         files = settings.get_data_file_paths()
@@ -78,7 +88,7 @@ class Stats:
         GAMEPKS = schedules[schedules["gameDate"] >= self.data_since].gamePk.unique()
         return GAMEPKS
 
-    def _sub_df_transform(player_id, splits):
+    def _sub_df_transform(self, player_id, splits):
         df = pd.json_normalize(splits, sep="_")
         df["player_id"] = player_id
         return df
